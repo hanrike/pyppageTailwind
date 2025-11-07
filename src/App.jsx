f6f1ca2d4b9d8dcc101a4d9a6fb255b121991b93
Jarol <jaancastano@utp.edu.co>
2025-11-07 06:44:40 -0500

    primer commit con tailwind funcionando

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-8xl text-amber-300'>
+        hola jarol
+      </div>
+      
+    </>
+  )
+}
+
+export default App
