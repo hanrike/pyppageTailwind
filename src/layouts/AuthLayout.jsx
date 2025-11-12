52413509d48416e5695900884a919bb39d037a74
Jarol <jaancastano@utp.edu.co>
2025-11-12 17:51:47 -0500

    rutas funcionando con su privateLayout, PublicLayout y AuthLayout con estilos tailwind

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import React from 'react'
 
 const AuthLayout = () => {
+  //Debo importar el outlet para que con el react router dom queden quemados los layouts con las rutas especificas en este caso registro
+  // y login
   return (
-    <div>
-        Autenticacion
-    </div>
+    <main className='flex-grow bg-blue-400'>
+      <Outlet/>
+    </main>
   )
 }
 
--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,10 +1,18 @@
 import { Outlet } from "react-router-dom";
 import React from 'react'
+import Sidebar from "../components/Sidebar";
+
 
 const PrivateLayout = () => {
+  //Debo importar el oulet para que con el react router dom queden quemados los layouts con las rutas especificas en este caso index,
+  //vehiculos y clientes
   return (
-    <div>
-      Private Layout
+    //con esta clase divido el sidebar de todos los private layout de admin clientes y vehiculos con sus rutas funcionando
+    <div className="flex w-screen h-screen">
+      <Sidebar/>
+      <main className='flex-grow bg-blue-400'>
+        <Outlet/>
+      </main>
     </div>
   )
 }
