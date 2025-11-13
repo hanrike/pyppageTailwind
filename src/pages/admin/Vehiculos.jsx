d6f72d935c1d0589a6bc7bf43da7a6307cc63af0
Jarol <jaancastano@utp.edu.co>
2025-11-13 17:41:41 -0500

    admin vehiculos creacion de formulario y boton con estilos tailwind

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Vehiculos = () => {
   return (
-    <div>
-      Administracion de vehiculos
-    </div>
+    <form className='flex flex-col'>
+      <h2>Formulario para creacion de vehiculos</h2>
+      <input type='text' placeholder='Nombre vehiculo'/>
+      <input type='text' placeholder='Marca Vehiculo'/>
+      <input type='text' placeholder='Modelo vehiculo'/>
+      <button className='bg-indigo-500 text-white'>Enviar Datos</button>
+    </form>
   )
 }
 
