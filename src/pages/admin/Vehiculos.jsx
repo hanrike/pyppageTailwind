c2e0e0794e9cbf2d86a7753ccde4b59298c0d395
Jarol <jaancastano@utp.edu.co>
2025-11-13 19:43:34 -0500

    forma de comprobar os arrow funtion =()=>{} en el console.log de forma general asi funciona pero no es la forma mas rapida de escribirlo

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,10 +1,14 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useState} from 'react';
 //importacion de la funcion useEFFect con el fin de que el codigo pueda estar pendiente de lo que el ususario va a realizar primero 
 //no como en python que todo es secuencial aqui se puede ejecutar de primero cualquier accion
+//funcion useState nos permite definir las variables que vamos a utilizar dentro de nustra funcion
 
 
 //aqui vienen las interactividades de la funcion useEffect
 const Vehiculos=()=>{
+ //de esta manera llamo al useState una variable dentro de la aplicacion que esta constantemente cambiando eso es un estado
+ const[nombreVehiculo,setNombreVehiculo]=useState(''); //el set es para poder cambiar el nombre de ese vehiculo es como en java setters y getters
+
   useEffect(()=>{
     console.log(
       'Hola soy un use effect que se ejecuta solo una vez cuando la pagina se renderiza, porque tiene el array de dependencias vacio');
@@ -12,12 +16,18 @@ const Vehiculos=()=>{
     //paso3 
     //paso etc
   },[]);
-
+   const cambioDeNombre = (e) => { //es una funcion que me permite cambiar los estados y los imputs cuando el ususario utiliza la pagina
+    console.log('nombre:',e.target.value)}//con console.log imprimo lo que el ususario ingresa en el nombre vehiculo del formulario
+    
+   const cambioDeMarca = (e) => { //es una funcion que me permite cambiar los estados y los imputs cuando el ususario utiliza la pagina
+    console.log('marca:e',e.target.value)//con console.log imprimo lo que el ususario ingresa en el nombre vehiculo del formulario
+     
+  }
   return (
     <form className='flex flex-col'>
       <h2>Formulario para creacion de vehiculos</h2>
-      <input type='text' placeholder='Nombre vehiculo'/>
-      <input type='text' placeholder='Marca Vehiculo'/>
+      <input onChange={cambioDeNombre} type='text' placeholder='Nombre vehiculo'/> {/** el evento  onChange cada que el input cambie se puede ejecutar una funcion */}
+      <input onChange={cambioDeMarca} type='text' placeholder='Marca Vehiculo'/> {/** como se ve en el console.log los valores cambian cuando se ponen los inputs */}
       <input type='text' placeholder='Modelo vehiculo'/>
       <button className='bg-indigo-500 text-white'>Enviar Datos</button>
     </form>
