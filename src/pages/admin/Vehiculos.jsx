87720a1791964d2a2012afb9a9fbbf7ecedace63
Jarol <jaancastano@utp.edu.co>
2025-11-13 20:25:41 -0500

    como realizar evento onChange y evento OnClick funcionando en la consola desarrrollador simulando envio de informacion al backend

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -7,7 +7,8 @@ import React,{useEffect,useState} from 'react';
 //aqui vienen las interactividades de la funcion useEffect
 const Vehiculos=()=>{
  //de esta manera llamo al useState una variable dentro de la aplicacion que esta constantemente cambiando eso es un estado
- const[nombreVehiculo,setNombreVehiculo]=useState(''); //el set es para poder cambiar el nombre de ese vehiculo es como en java setters y getters
+ const[nombreVehiculo,setNombreVehiculo]=useState(''); //el set es para poder cambiar el nombre de ese vehiculo es como en java setters y getters getter leer informacion de la variable 
+                                                        //seter cambiar informacion de la variable
 
   useEffect(()=>{
     console.log(
@@ -16,14 +17,19 @@ const Vehiculos=()=>{
     //paso3 
     //paso etc
   },[]);
+    const enviarDatosAlBackend = (params) => {
+      console.log('El valor de la variable nombre vehiculo es',nombreVehiculo)
+    }
+    
     
   return (
     <form className='flex flex-col'>
       <h2>Formulario para creacion de vehiculos</h2>
-      <input onChange={(e)=>{console.log('nombre:',e.target.value)}} type='text' placeholder='Nombre vehiculo'/> {/** el evento  onChange cada que el input cambie se puede ejecutar una funcion */}
+      {/** onChange sirve para recibir el valor de un evento cualquieraque es lo que esta ingresando en el formulario */}
+      <input onChange={(e)=>{setNombreVehiculo (e.target.value)}} type='text' placeholder='Nombre vehiculo'/> {/** el evento  onChange cada que el input cambie se puede ejecutar una funcion */}
       <input onChange={(e)=>{console.log('marca:',e.target.value)}} type='text' placeholder='Marca Vehiculo'/> {/** como se ve en el console.log los valores cambian cuando se ponen los inputs */}
       <input type='text' placeholder='Modelo vehiculo'/>
-      <button className='bg-indigo-500 text-white'>Enviar Datos</button>
+      <button type='button' onClick={enviarDatosAlBackend} className='bg-indigo-500 text-white'>Enviar Datos</button> {/** onClick  para ejecutar una funcion o accion que quiero que se ejecute cunado hago click al boton*/}
     </form>
   )
 };
