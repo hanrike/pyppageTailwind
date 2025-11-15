030dae251e67abb664080d3e6022d69ddcd431eb
Jarol <jaancastano@utp.edu.co>
2025-11-15 18:30:10 -0500

    renderizacion con el boton cambiando tambien de estado se hizo con un useEffcet

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -7,13 +7,23 @@ import React,{useEffect,useState} from 'react';
 //Lo que quiero es ir a rutas pero esta vez utilizando renderizacion condicional y no a traves del router dom como ya se vio
 const Vehiculos = () => {
   const[mostrarTabla,setMostrarTabla]=useState(true);
+  const[textoBoton,setTextoBoton]=useState('')
+ //creacion de useEffect con el fin de que el boton pueda cambiar tambien cuando se hace el evento click 
+  useEffect(()=>{
+    if (mostrarTabla){
+      setTextoBoton('Crear nuevo Vehiculo');
+    }else{
+      setTextoBoton('Mostrar todos los vehiculos')
+    }
+  },[mostrarTabla]);
   return(
     <div>
       <h2>Pagina de administracion de vehiculos</h2>
       {/**con el evento onClick logro que cuando hago click en el boton me muestre la funcion TablaVehiculos o la funcion FormularioCreacionVehiculos */}
       <button onClick={()=>{
         setMostrarTabla(!mostrarTabla)
-      }} className='text-white bg-indigo-500 p-5'>Crear nuevo vehiculo</button>{/**estilo tailwind del boton */}
+      }} className='text-white bg-indigo-500 p-5'>
+        {textoBoton}</button> {/**de esta manera se puede ejecutar codigo javascript {textoBoton} */}{/**estilo tailwind del boton */}
       {mostrarTabla ? <TablaVehiculos/>:<FormularioCreacionVehiculos/> } {/**si mostrarTabla es verdadero entonces habilite  */}
       {/**renderizacion para el componente de tabla vehiculos */}
     </div>
