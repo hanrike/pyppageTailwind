7ddcb7f8d831d11bc1586bef05ad06016bc3f82a
Jarol <jaancastano@utp.edu.co>
2025-11-15 18:07:47 -0500

    renderizacion condicional cuando hago click en el boton crear nuevo vehiculo se cativa la funcion TablaVehiculos y la funcion Formulario...todavia el boton no cambia segun la funcion

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -3,48 +3,38 @@ import React,{useEffect,useState} from 'react';
 //no como en python que todo es secuencial aqui se puede ejecutar de primero cualquier accion
 //funcion useState nos permite definir las variables que vamos a utilizar dentro de nustra funcion
 
-//EJERCICIO DE PRUEBA REALIZAR UN FORMULARIO QUE LE PIDA AL USUSARIO LA EDAD Y MUSTRE UN MENSAJE QUE LE DIGA SI ES MAYOR DE EDAD O NO EJMPLO RAPIDO DE UNA RENDERIZACION
 
-//aqui vienen las interactividades de la funcion useEffect
-const Vehiculos=()=>{
-    const [edad,setEdad]=useState(0);
-    const [esMenorDeEdad,setEsMenorDeEdad]=useState(false) //para la renderizacion condicional se hace la funcion const useState y la inicializo en falso para que no aparezca inicialmente
-    const [mostrarCamposAdicionales,setMostrarCamposAdicionales]=useState(false)
-  useEffect(()=>{  //El useeFFECT ME SIRVE PARA HACER LOS CONTROLES Q yo necesite
-    if(edad>=18){ //si edad es mayor a 18 setEsMenorDeEdad(false) si no setEsMenorDeEdad(true)
-      setEsMenorDeEdad(false)
-      }else{
-      setEsMenorDeEdad(true)
-    }
-  },[edad])
-    
-     
-  return (
-    <form className='flex flex-col'>
-      <h2>Formulario para creacion de vehiculos</h2>
-      <label htmlFor='edad'>
-      Por favor ingrese su edad
-      <input value={edad} onChange={(e)=>{setEdad(e.target.value)}} className='border-gray-600' name='edad' type='number'/>
-      </label>
-      {esMenorDeEdad ? ( //renderizacion condicional en react es: si es esMenorDeEdad (?)=es menor de edad y el else (:) si no no puede hacer pagos esta es la sintaxis
-        <span className='text-3xl text-red-500'>Usted es menor de edad no puede hacer pagos</span>
-      ):(
-        <span className='text-3xl text-green-500'>Usted es menor de edad no puede hacer pagos</span>
-      )} 
-      <button onClick={()=>setMostrarCamposAdicionales(!mostrarCamposAdicionales)} type='button' className='text-white bg-indigo-500'>
-        Mostrar campos adicionales
-      </button>
-      {mostrarCamposAdicionales && (
-        <div>
-          <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text'/>
-          <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text'/>
-          <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text'/>
-          <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text'/>
-          <input className='border bg-gray-400 my-2 p-3' placeholder='dato nuevo' type='text'/>
-        </div>
-      )}
-    </form>
+//Lo que quiero es ir a rutas pero esta vez utilizando renderizacion condicional y no a traves del router dom como ya se vio
+const Vehiculos = () => {
+  const[mostrarTabla,setMostrarTabla]=useState(true);
+  return(
+    <div>
+      <h2>Pagina de administracion de vehiculos</h2>
+      {/**con el evento onClick logro que cuando hago click en el boton me muestre la funcion TablaVehiculos o la funcion FormularioCreacionVehiculos */}
+      <button onClick={()=>{
+        setMostrarTabla(!mostrarTabla)
+      }} className='text-white bg-indigo-500 p-5'>Crear nuevo vehiculo</button>{/**estilo tailwind del boton */}
+      {mostrarTabla ? <TablaVehiculos/>:<FormularioCreacionVehiculos/> } {/**si mostrarTabla es verdadero entonces habilite  */}
+      {/**renderizacion para el componente de tabla vehiculos */}
+    </div>
   )
-};
+}
+
+const TablaVehiculos = () => {
+  return(
+    <div>
+      Esto es un div pero deberia ser una tabla mostrando todos losd vehiculos
+    </div>
+  )
+}
+
+const FormularioCreacionVehiculos = () => {
+  return(
+    <div>
+      Esto es un div pero deberia ser un formulario para creacion de un nuevo vehiculo
+    </div>
+  )
+}
+
 
 export default Vehiculos;
