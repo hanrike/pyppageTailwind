3785b17e9e27871e55284a862d26426526cfe8f3
Jarol <jaancastano@utp.edu.co>
2025-12-02 19:22:58 -0500

    utilizando el componente ruta pa duplicar botones en sidebar funcionando

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,17 +13,27 @@ const Sidebar = () => {
       <ImagenLogo/>
     </Link>
     <button>Perfil</button>
-    <Link to='/admin/vehiculos'>{/* Importando este link ya tengo navegabilidad en mi boton en este caso voy a daminiustracion de vehiculos */}
-    < button className='p1 bg-indigo-700 hover:bg-indigo-900 flex w-full items-center text-white rounded-md'>
-    <Projector size={20} className="text-white w-10" />
-      Vehiculos
-    </button>
-    </Link>
+      <Ruta/> {/* pongo la ruta aqui con el componente que cree llamado tuta  */}
+      <Ruta/>
+      <Ruta/>
     <button>Admnistracion de Vehiculos</button>
     <button>Admnistracion de Vehiculos</button>
     <button>Cerrar Sesion </button>
   </nav>
   )
 };
+
+//voy a crear un componente para los botones del sidebar 
+const Ruta = () => {
+  return(
+    <Link to='/admin/vehiculos'>{/* Importando este link ya tengo navegabilidad en mi boton en este caso voy a daminiustracion de vehiculos */}
+    < button className='p1 bg-indigo-700 hover:bg-indigo-900 flex w-full items-center text-white rounded-md'>
+    <Projector size={20} className="text-white w-10" />
+      Vehiculos
+    </button>
+    </Link>
+  )
+}
+
 
 export default Sidebar;
