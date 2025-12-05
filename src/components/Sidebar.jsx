726f162d2e3ec3159305e780317895136d1c5197
Jarol <jaancastano@utp.edu.co>
2025-12-05 17:50:48 -0500

    sidebar funcionando por ahora queda fijo el color donde este ubicado la direccion solo esta funcionando la rurta vehiculos

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { Link,useLocation } from 'react-router-dom';
 import ImagenLogo from '../components/ImagenLogo'
 import { Projector,ChartSpline,Users,User,ArrowDownNarrowWide } from 'lucide-react';
 
@@ -30,9 +30,26 @@ const Sidebar = () => {
 //voy a crear un componente para los botones del sidebar 
 //le estoy pasando icono,ruta,nombre como props 
 const Ruta = ({icono,ruta,nombre}) => {
+  //todo lo que devuelven los hooks como estos son funciones o estados, en este caso este devuelve un objeto q es un estado
+  const location=useLocation()
+  const[isActive,setIsActive]=useState(false)
+
+  useEffect(()=>{
+    console.log(location,ruta)
+    if(location.pathname===ruta){
+      setIsActive(true)
+    }else{
+      setIsActive(false)
+    }
+  },[location,ruta])
+  //useEffect para hacer prueba de consola de porque al profe no le esta funcionando
+  useEffect(()=>{
+    console.log(isActive,ruta)
+  },[isActive,ruta])
   return(
     <Link to={ruta} className='w-full'>{/* Importando este link ya tengo navegabilidad en mi boton en este caso voy a daminiustracion de vehiculos */}
-    < button className='p1 my-2 bg-indigo-700 hover:bg-indigo-900 flex w-full items-center justify-center gap-2 text-white rounded-md'>
+    < button 
+    className={`p1 my-2 bg-${isActive ? 'indigo':'gray'}-700 hover:bg-indigo-900 flex w-full items-center justify-center gap-2 text-white rounded-md`}>{/* ponemos el stringliteral con el fin de poner una variable */}
     {/* CORRECTO: Renderizamos el componente directamente se renderiza directamente */}
         {icono} 
     {/* Renderizamos el nombre */}
